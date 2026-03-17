282d39dd29ccfca085eb8504a18d8875f77cbbd9
Firdavs <f27629462@gmailcom>
2026-03-17 19:48:33 +0500

    Home sahifasi bajarildi

---
+++ src/pages/home/HomePage.jsx
@@ -0,0 +1,13 @@
+import MyCarousel from "./HomePageCarousel";
+import Products from "../products/Products";
+
+export default function HomePage() {
+    return (
+        <div className="home-page">
+           <MyCarousel/>
+           <h2>Bizda mavjud bo'lgan mahsulotlar</h2>
+         <Products/>
+        </div>
+        
+    );
+}
---
+++ src/pages/home/HomePageCarousel.jsx
@@ -0,0 +1,68 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Navigation, Pagination, EffectFade } from 'swiper/modules';
+
+// Swiper stillarini import qilamiz
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/effect-fade';
+
+// O'zimizning alohida CSS faylimiz
+import './Style/HomeStyle.css';
+
+const MyCarousel = () => {
+  const slides = [
+    {
+      id: 1,
+      image: "https://c4.wallpaperflare.com/wallpaper/264/666/478/3-316-16-9-aspect-ratio-s-sfw-wallpaper-preview.jpg",
+      title: "Sizning Birinchi Slaydingiz",
+      description: "Zamonaviy dizayn va yuqori sifat"
+    },
+    {
+      id: 2,
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhUVIod9IAKohXLG6TnxyHwt3VDoyeripSPg&s",
+      title: "Ikkinchi Maxsus Taklif",
+      description: "Faqat bizda eng yaxshi narxlar"
+    },
+    {
+      id: 3,
+      image: "https://c4.wallpaperflare.com/wallpaper/266/880/964/3-316-16-9-aspect-ratio-s-sfw-wallpaper-preview.jpg",
+      title: "Yangi To'plam",
+      description: "O'z uslubingizni biz bilan toping"
+    }
+  ];
+
+  return (
+    <div className="carousel-container">
+      <Swiper
+        modules={[Autoplay, Navigation, Pagination, EffectFade]}
+        effect={'fade'} // Yumshoq o'tish effekti
+        speed={1000}
+        loop={true}
+        autoplay={{
+          delay: 4000,
+          disableOnInteraction: false,
+        }}
+        pagination={{ clickable: true }}
+        navigation={true}
+        className="mySwiper"
+      >
+        {slides.map((slide) => (
+          <SwiperSlide key={slide.id}>
+            <div className="slide-content">
+              <img className='carousel-img' src={slide.image} alt={slide.title} />
+              <div className="image-overlay">
+                <div className="text-content">
+                  <h2>{slide.title}</h2>
+                  <p>{slide.description}</p>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default MyCarousel;
