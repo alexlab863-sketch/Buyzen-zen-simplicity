10e399a69cf6a492c4863b7aaa29e2453c00b5f6
Firdavs <f27629462@gmailcom>
2026-03-17 19:47:31 +0500

    Error page qo'shildi

---
+++ src/pages/ErrorPage/NotFound.jsx
@@ -0,0 +1,12 @@
+import { useNavigate } from "react-router-dom"
+
+export default function NotFound(){
+    const navigate = useNavigate()
+    return(
+        <div className="not-found">
+            <h1>404 </h1>
+            <p>The page you are looking for does not exist.</p>
+            <button onClick={() => navigate("/")}>Go to Home</button>
+        </div>
+    )
+}
