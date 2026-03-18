b15c69902132ecb5835a8672a2ad61a7b12406cd
Firdavs <f27629462@gmailcom>
2026-03-18 15:52:31 +0500

    Pagination muvaffaqiyatli bo'ldi

--- src/pages/products/Card.jsx
+++ src/pages/products/Card.jsx
@@ -1,17 +1,18 @@
 import "./Style/ProductStyle.css";
 
-export default function Card({ products }) {
+export default function Card({ products }) { 
     return (
         <div className="card-container"> 
-            {products.map(product => (
-                <li key={product.id} className="card-item"> 
-                    <img src={product.image} alt={product.title}  className="card-image" />
+            {products && products.map(product => (
+                <div key={product.id} className="card-item"> 
+                    <img src={product.thumbnail} alt={product.title} className="card-image" />
                     <div className="card-content"> 
                         <h3 className="card-title">{product.title}</h3> 
+                        <p className="card-brand">{product.brand}</p>
                         <p className="card-price">Narx: ${product.price}</p> 
-                       
                     </div>
-                </li>
+                    <button className="card-btn">Batafsil</button>
+                </div>
             ))}
         </div>
     );
